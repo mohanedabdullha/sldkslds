731abeb02bb1be14bc206fddfe56d17a2f9b0961
mohanedabdullha <43791989+mohanedabdullha@users.noreply.github.com>
2018-11-07 21:14:17 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("508619864487886858");
+const mohamed= member.guild.channels.get("509745217285652513");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`Welcome To Server **Magic**`), 4000)        
